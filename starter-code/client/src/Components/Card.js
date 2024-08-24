44c6a0c3454c6d3f309764895d3e8f5a7c29ce70
Asangabia Mole <moleasangabia23@gmail.com>
2024-08-24 17:59:17 +0000

    cards with array data

--- starter-code/client/src/Components/Card.js
+++ starter-code/client/src/Components/Card.js
@@ -1,8 +1,26 @@
 import React from "react";
 import "./Card.css";
+import Data from "../data.json";
 
 function Card() {
-  return <div>Card</div>;
+  return (
+    <div className="card__container">
+      {Data.map((item, index) => {
+        return (
+          <div className="card" key={index}>
+            <img src={item.logo} alt="logo" />
+            <div className="card__info">
+              <span>{item.postedAt} . </span>
+              <span>{item.contract}</span>
+              <h3>{item.position}</h3>
+              <p>{item.company}</p>
+              <p>{item.location}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Card;
--- starter-code/client/src/Components/HomePage.js
+++ starter-code/client/src/Components/HomePage.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import "./HomePage.css"
 import Heading from "./Heading.js"
+import Card from "./Card.js"
 
 function HomePage() {
   return (
     <div className="container">
         <Heading />
+        <Card/>
     </div>
   )
 }
